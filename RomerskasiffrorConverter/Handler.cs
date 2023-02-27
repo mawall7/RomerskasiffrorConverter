b32eb0cd744af608e3ae40ef8a8738a6d871db24
Mattias Wallerman <wallermanmattias@yahoo.se>
2023-02-27 10:06:42 +0100

    Add project files.

---
+++ RomerskasiffrorConverter/Handler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RomerskasiffrorConverter
+{
+   public class Handler
+    {
+        private Dictionary<string, int> RomannumbersDict { get; set; }
+        public void GetDictValue(string key, Dictionary<string, int> dict)
+        {
+            Console.WriteLine(dict[key]);
+        }
+
+        public static Handler Createhandlerclass()
+        {
+            return new Handler();
+        }
+
+        public void CreateNewDict() 
+        {
+            RomannumbersDict = new Dictionary<string, int>()
+            {
+                {"I",1 },
+                {"V", 5 },
+                {"X", 10 },
+                {"L", 50 },
+                {"C",100 },
+                {"D", 500 },
+                {"M", 1000 }
+            };
+        }
+
+        public void Present()
+        {
+            foreach (var item in RomannumbersDict)
+            {
+                Console.WriteLine(item.Key);
+            }
+        }
+        public int GetResultValue(string s)
+        {
+            return RomannumbersDict[s];
+        }
+    }
+}
---
+++ RomerskasiffrorConverter/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+//Romerska siffror converter. AddKeys() populerar en lista med de numeriska värdena för de romerska siffrorna som översätts av en dictionary som är private i klasssen Handler. Compared results() metoden sköter logiken (för addition eller subtraktion).  
+namespace RomerskasiffrorConverter
+{
+    class Program
+    {
+                
+        public static void Main(string[] args)
+        {
+            int? results = 0;
+            string keys = null;
+
+
+
+            //Handler newHandler = new Handler();
+            Handler newHandler = Handler.Createhandlerclass();
+            newHandler.CreateNewDict();
+            List<int> numbers = new List<int>();
+           
+            Console.Write("Romerska siffror-Converter 2023\nSkriv ett romerskt tal: ");
+            
+            //Readkey
+            while (string.IsNullOrWhiteSpace(keys))
+            {
+                keys = Console.ReadLine().ToUpper();
+                
+            }
+
+            
+            foreach (int item in AddKeys(keys, newHandler))
+            {
+               
+                numbers.Add(item);
+                                
+            }
+            
+                
+             for (int i = numbers.Count - 1; i >= 0; i--)
+             {
+                int? Compared = Comparedresults(i, i - 1, numbers);
+                if (Compared != null)
+                {
+                    results = results + Compared;
+                    --i;
+                }
+                else if (Compared == null)
+                    results = results + numbers[i];
+
+            }
+
+            Console.WriteLine(results);
+            
+            
+            Console.ReadKey();
+        }
+
+        private static int? Comparedresults(int i, int v, List<int>numbers)
+        {
+            int? result;
+            int y = 0; 
+            if(v>=0) y = numbers[v];
+            int x = numbers[i]; 
+            result = y > x ? x + y :  x - y;
+            if (x == y) result = null;
+            return result;
+            
+        }
+        public static IEnumerable AddKeys(string k, Handler h)
+        {
+            //h.CreateNewDict();
+            for (int i = 0; i < k.Length; i++)
+            {
+                yield return h.GetResultValue(k[i].ToString());
+
+            }
+          
+        }
+    }
+
+
+}
